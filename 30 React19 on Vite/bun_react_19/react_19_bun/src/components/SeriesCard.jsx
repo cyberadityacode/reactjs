d260bee6b5eb362996d1f1c0f4e8cf74ffff91b9
Aditya Dubey <adityadubey793@gmail.com>
2025-05-11 21:19:30 +0530

    added styled component

--- 30 React19 on Vite/bun_react_19/react_19_bun/src/components/SeriesCard.jsx
+++ 30 React19 on Vite/bun_react_19/react_19_bun/src/components/SeriesCard.jsx
@@ -1,21 +1,39 @@
-import styles from './Netflix.module.css';
+import styles from "./Netflix.module.css";
+import styled from "styled-components";
 
+
+  const ButtonWatchNow = styled.button`
+    padding: 1.2rem 2.4rem;
+    border: none;
+    font-size: 1.6rem;
+    background-color: ${(props) =>
+       props.$rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+    color: var(--bg-color);
+    font-weight: bold;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+  &:hover {
+    opacity: 0.9;
+    transform: scale(1.03);
+  }
+`;
+
+const Rating = styled.h3`
+    font-size : 1.6rem;
+    color:#7dcea0;
+    text-transform: capitalize;
+`
 
 export const SeriesCard = (props) => {
   console.log("Check Props- ", props);
   // destructuring
   const { img_url, name, rating, description, genre, watch_url } = props.data;
-  const buttonStyle = {
-    padding: "1.2rem 2.4rem",
-    border: "none",
-    fontSize: "1.6rem",
-    backgroundColor: `${rating>=8.5 ? "#7dcea0":"#f7dc6f"}`,
-    color: "var(--bg-color)",
-    fontWeight: "bold",
-    cursor: "pointer"
-  };
+  // const buttonStyle = {}
 
-  const ratingClass = rating>=8.5 ? styles.super_hit : styles.average;
+
+  // conditional css class
+  const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
 
   return (
     <li className={styles.card}>
@@ -23,12 +41,16 @@ export const SeriesCard = (props) => {
         <img src={img_url} width="200px" height="200px" alt="" />
       </div>
       <div className={styles["card-content"]}>
-        <h2 >Name: {name} </h2>
-        <h3 >Rating: <span className={`${styles.rating} ${ratingClass}`}>{rating}</span> </h3>
+        <h2>Name: {name} </h2>
+        <Rating>
+          Rating:{" "}
+          <span className={`${styles.rating} ${ratingClass}`}>{rating}</span>{" "}
+        </Rating>
         <p>Summary: {description}</p>
         <p>Genre {genre.join(", ")}</p>
-        <a target="_blank" href={watch_url}>
-          <button style={buttonStyle}>Watch Now </button>
+        <a target="_blank" rel="noreferrer"  href={watch_url}>
+          {/* <button style={buttonStyle}>Watch Now </button> */}
+          <ButtonWatchNow $rating={rating}>Watch Now </ButtonWatchNow>
         </a>
       </div>
     </li>
