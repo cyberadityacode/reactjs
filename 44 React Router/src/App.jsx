2f9db994a6a2e065c794e4f25433b8cc102567e6
Aditya Dubey <adityadubey793@gmail.com>
2025-05-22 22:07:59 +0530

    add React router with new and old way

---
+++ 44 React Router/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
+import Contact from '../src/pages/Contact'
+import Movie from '../src/pages/Movie'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from 'react-router-dom'
+
+export default function App() {
+
+  // Latest Way
+/*   const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home />
+    },
+    {
+      path:"/movie",
+      element: <Movie />
+    },
+    {
+      path: "/about",
+      element:<About />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    }
+  ]) */
+  
+    // Old way
+
+    const router = createBrowserRouter(
+      createRoutesFromElements(
+        // <Route>
+        <>
+          <Route path='/'  element = {<Home/>} />
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/movie' element={<Movie />}/>
+          </>
+        //</Route> 
+      )
+    )
+  
+  
+    return (
+    <RouterProvider router={router} />
+  )
+}
