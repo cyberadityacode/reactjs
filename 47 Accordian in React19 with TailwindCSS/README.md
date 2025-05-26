ebeaf4141c2dbd511c6727cb5d245e64543d09a0
Aditya Dubey <adityadubey793@gmail.com>
2025-05-26 07:20:47 +0530

    started working on accordian

---
+++ 47 Accordian in React19 with TailwindCSS/README.md
@@ -0,0 +1,1 @@
+# Accordian in React 19 with TailwindCSS
---
+++ 47 Accordian in React19 with TailwindCSS/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div >
+      <h1 className='text-2xl'>Accordian in React19</h1>
+    </div>
+  )
+}
