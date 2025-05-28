7e23fae9143fb30f70166ccc9e8180b324e65920
Aditya Dubey <adityadubey793@gmail.com>
2025-05-28 20:29:13 +0530

    added React query way for data fetching

--- 52 TanStack React Query/src/pages/FetchRQ.jsx
+++ 52 TanStack React Query/src/pages/FetchRQ.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { fetchPosts } from '../API/TraditionalAPI';
 
 export default function FetchRQ() {
+
+  const getPostsData =async()=>{
+    try{
+      const res = await fetchPosts();
+      console.log(res.data);
+
+      return res.status===200? res.data: []
+    }catch(error){
+      console.error(error)
+      return [];
+    }
+  }
+  const {data} =  useQuery({
+    queryKey: ['posts'], //reminds me of useState
+    queryFn: getPostsData, //reminds me of useEffect
+  })
+
   return (
-    <div>FetchRQ</div>
+    <div>
+      <h1>React Query Way to Fetch Data</h1>
+      <ul>
+        {data?.map((currentPost) => {
+          return <li key={currentPost.id}>{currentPost.body}</li>;
+        })}
+      </ul>
+    </div>
   )
 }
