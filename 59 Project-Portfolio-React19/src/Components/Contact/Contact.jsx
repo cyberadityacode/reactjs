b4527b4adf3c6df51715b0581f44292958d7d09b
Aditya Dubey <adityadubey793@gmail.com>
2025-05-31 11:04:37 +0530

    update project portfolio - responsiveness

--- 59 Project-Portfolio-React19/src/Components/Contact/Contact.jsx
+++ 59 Project-Portfolio-React19/src/Components/Contact/Contact.jsx
@@ -3,9 +3,33 @@ import theme_pattern from '../../assets/theme_pattern.svg';
 import mail_icon from '../../assets/mail_icon.svg';
 import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
+import { useState } from 'react';
 
 
 export default function Contact() {
+    const [result, setResult] = useState("");
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", import.meta.env.VITE_API_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className='contact' id='contact'>
         <div className='contact-title'>
@@ -31,7 +55,7 @@ export default function Contact() {
                     </div>
                 </div>
             </div>
-            <form className="contact-right">
+            <form onSubmit={onSubmit} className="contact-right">
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='Enter your name' name='name'/>
 
@@ -43,6 +67,7 @@ export default function Contact() {
 
                 <button type='submit' className='contact-submit'>Submit Now</button>
             </form>
+              <span>{result}</span>
         </div>
     </div>
   )
--- 59 Project-Portfolio-React19/src/Components/Navbar/NavBar.jsx
+++ 59 Project-Portfolio-React19/src/Components/Navbar/NavBar.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './NavBar.css';
 import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg';
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 
 export default function NavBar() {
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = ()=>{
+    menuRef.current.style.right ="0px"; 
+  }
+  const closeMenu = ()=>{
+    menuRef.current.style.right ="-350px"; 
+  }
   return (
     <div className='navbar'>
         <img src={logo} alt="Logo" />
-        <ul className="nav-menu">
+        <img src={menu_open} alt="" className='nav-mob-open' onClick={openMenu}  />
+        <ul ref={menuRef} className="nav-menu">
+          <img src={menu_close} className='nav-mob-close' onClick={closeMenu} alt="" />
             <li><AnchorLink className="anchor-link" href="#home"><p onClick={()=>setMenu("home")}>Home</p> </AnchorLink> {menu==="home"?<img src={underline} alt='home' />: <></>}</li>
             <li><AnchorLink className="anchor-link" offset={50} href="#about"><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={underline} alt='about' />: <></>} </li>
             <li><AnchorLink className="anchor-link" offset={50} href="#services"><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<img src={underline} alt='services' />: <></>}</li>
