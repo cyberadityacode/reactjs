6b61c2c030e439b6a7598f7e5ddecea33c0e0ba1
Aditya Dubey <adityadubey793@gmail.com>
2025-06-02 09:49:05 +0530

    added context api

--- 61 Redux 01 Redux and Prerequisite/src/App.jsx
+++ 61 Redux 01 Redux and Prerequisite/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import UseReducerFirst from './components/useReducer01/UseReducerFirst'
-import UseReducerSecond from './components/useReducer01/UseReducerSecond'
+import React from "react";
+import UseReducerFirst from "./components/useReducer01/UseReducerFirst";
+import UseReducerSecond from "./components/useReducer01/UseReducerSecond";
+import { BioProvider } from "./components/useContext/UseContextFirst";
+import About from "./components/About";
+import SomeComponent from "./components/SomeComponent";
 
 export default function App() {
   return (
-    // <UseReducerFirst />
-    <UseReducerSecond />
-  )
+    <>
+       <UseReducerFirst />
+      <UseReducerSecond />
+      <BioProvider>
+        <About />
+      <SomeComponent />
+      </BioProvider>
+      {/* <SomeComponent /> */}
+
+    </>
+  );
 }
