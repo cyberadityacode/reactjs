25f7b045bd37bbebce8093fd17fa07feb5861b10
Aditya Dubey <adityadubey793@gmail.com>
2025-06-04 17:02:36 +0530

    added dynamic input addition

--- 67 Redux Toolkit First Program/src/components/CounterFirst.jsx
+++ 67 Redux Toolkit First Program/src/components/CounterFirst.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   decrement,
@@ -7,16 +7,25 @@ import {
 } from "../features/counter/counterSlice";
 
 export default function CounterFirst() {
+  const [amount, setAmount] = useState(1);
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
+  const handleAmountChange = (e) => {
+    setAmount(Number(e.target.value));
+  };
   return (
     <div>
       <h1>Redux RTK Counter {count}</h1>
       <button onClick={() => dispatch(increment())}>Increase</button>
       <button onClick={() => dispatch(decrement())}>Decrease</button>
+
+      <input type="number" value={amount} onChange={handleAmountChange} />
       <button onClick={() => dispatch(incrementByAmount(7))}>
         Increment by 7
+      </button>
+      <button onClick={() => dispatch(incrementByAmount(amount))}>
+        Increment by {amount}
       </button>
     </div>
   );
