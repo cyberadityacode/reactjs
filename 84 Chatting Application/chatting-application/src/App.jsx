357efa2a202d23e66f368106f9bf3790f23140cd
Aditya Dubey <adityadubey793@gmail.com>
2025-07-26 15:13:44 +0530

    just for fun added dark mode toggle

--- 84 Chatting Application/chatting-application/src/App.jsx
+++ 84 Chatting Application/chatting-application/src/App.jsx
@@ -3,15 +3,24 @@ import ChatRoom from "./components/ChatRoom";
 
 const App = () => {
   const [userId, setUserId] = useState("user1");
+  const [currentOn, setCurrentOn] = useState("On");
 
+  const toggleDarkMode = () => {
+    setCurrentOn((prev) => (prev === "On" ? "Off" : "On"));
+  };
   const toggleUser = () => {
     setUserId((prev) => (prev === "user1" ? "user2" : "user1"));
   };
 
+  const divStyle = {
+    backgroundColor: currentOn === "On" ? "gray" : "white",
+  };
+
   return (
-    <div className="app">
+    <div className="app" style={divStyle}>
       <h1>Firebase Chat</h1>
       <button onClick={toggleUser}>Switch User (Current: {userId})</button>
+      <button onClick={toggleDarkMode}>Dark Mode Status {currentOn}</button>
       {/*  Use dynamic userId here */}
       <ChatRoom chatId="chat123" currentUserId={userId} />
     </div>
